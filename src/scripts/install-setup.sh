d84a22b76a90f55b7cc8cd8d06b7b4102d856ced
Drew Ayling <dsayling@gmail.com>
2020-10-14 12:22:16 -0600

    python fun

--- src/scripts/install-setup.sh
+++ src/scripts/install-setup.sh
@@ -2,20 +2,45 @@ if [[ $EUID == 0 ]]; then export SUDO=""; else # Check if we are root
   export SUDO="sudo";
 fi
 
+SetupPython() {
+    # setups pyenv on a debian based system
+    # these are the system level deps for pyevn install, these are offically recommneded for install by AWS
+    $SUDO apt-get -qq update > /dev/null
+    $SUDO apt-get -qq -y install build-essential zlib1g-dev libssl-dev libncurses-dev libffi-dev libsqlite3-dev libreadline-dev libbz2-dev
+    $SUDO apt-get -qq -y install python3-dev
+    SetupVirtualEnv
+}
+
+SetupVirtualEnv() {
+    if [ $(which pip | tail -1) ]; then
+        echo "pip found"
+    else
+        echo "pip not found"
+        $SUDO curl https://bootstrap.pypa.io/get-pip.py | python3
+    fi
+    pip install virtualenv
+}
+
+
+
 InstallEBCLI() {
     cd /tmp || { echo "Not able to access /tmp"; return; }
-
+    git clone https://github.com/aws/aws-elastic-beanstalk-cli-setup.git
     if uname -a | grep Darwin > /dev/null 2>&1; then
-        oit clone https://github.com/aws/aws-elastic-beanstalk-cli-setup.git
         brew install zlib openssl readline
         $SUDO CFLAGS="-I$(brew --prefix openssl)/include -I$(brew --prefix readline)/include -I$(xcrun --show-sdk-path)/usr/include" LDFLAGS="-L$(brew --prefix openssl)/lib -L$(brew --prefix readline)/lib -L$(brew --prefix zlib)/lib" ./aws-elastic-beanstalk-cli-setup/scripts/bundled_installer >/dev/null 2>&1
         return $?
     elif uname -a | grep Linux > /dev/null 2>&1; then
-        $SUDO apt-get -qq update > /dev/null
-        $SUDO apt-get -qq -y install build-essential zlib1g-dev libssl-dev libncurses-dev libffi-dev libsqlite3-dev libreadline-dev libbz2-dev
-        pip install pipx
-        pipx install awsebcli
+        PY=
+        if [ $(which python3 | tail -1) ]; then
+            echo "Python3 env found"
+            SetupVirtualEnv
+        else
+            echo "Python3 env not found, setting up python 3.7.9 with pyenv"
+            SetupPython
+        fi
     fi
+            python3 aws-elastic-beanstalk-cli-setup/scripts/ebcli_installer.py
 }
 
 CheckAWSEnvVars() {
