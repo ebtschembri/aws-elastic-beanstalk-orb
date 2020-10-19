5edde8303211c8545f104a869a004d058745c43b
Drew Ayling <dsayling@gmail.com>
2020-10-19 11:46:44 -0600

    killer bash

--- src/scripts/install-setup.sh
+++ src/scripts/install-setup.sh
@@ -26,9 +26,9 @@ SetupPipx() {
 
 
 InstallEBCLI() {
-    cd /tmp || { echo "Not able to access /tmp"; return; }
-    git clone https://github.com/aws/aws-elastic-beanstalk-cli-setup.git
     if uname -a | grep Darwin > /dev/null 2>&1; then
+        cd /tmp || { echo "Not able to access /tmp"; return; }
+        git clone https://github.com/aws/aws-elastic-beanstalk-cli-setup.git
         brew install zlib openssl readline
         CFLAGS="-I$(brew --prefix openssl)/include -I$(brew --prefix readline)/include -I$(xcrun --show-sdk-path)/usr/include" LDFLAGS="-L$(brew --prefix openssl)/lib -L$(brew --prefix readline)/lib -L$(brew --prefix zlib)/lib" ./aws-elastic-beanstalk-cli-setup/scripts/bundled_installer >/dev/null 2>&1
         return $?
@@ -38,10 +38,10 @@ InstallEBCLI() {
         $SUDO apt-get -qq -y install build-essential zlib1g-dev libssl-dev libncurses-dev libffi-dev libsqlite3-dev libreadline-dev libbz2-dev
         if [ "$(which python3 | tail -1)" ]; then
             echo "Python3 env found"
-            SetupVirtualEnv
+            SetupPipx
         else
             echo "Python3 env not found, setting up python with apt"
-            SetupPipx
+            SetupPython
         fi
     fi
         pipx install awsebcli
